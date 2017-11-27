d9097edd24dc365db125bbf690d832749cb675e7
Elmico <mikkotella@hotmail.com>
2017-11-28 01:40:23 +0200

    Add files via upload

---
+++ Esa/BL.cs
@@ -0,0 +1,28 @@
+﻿namespace Esa
+{
+    public class Ammattiala
+    {
+        //public int id { get; set; }
+        public string organisaatio { get; set; }
+        public string ammattiala { get; set; }
+        public string tyotehtava { get; set; }
+        //public string tyoavain { get; set; }
+        public string osoite { get; set; }
+        public string haku_paattyy_pvm { get; set; }
+        public string linkki { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0}, {0}, {0}, {0}, {0}, {0}", organisaatio, ammattiala, tyotehtava, osoite, haku_paattyy_pvm, linkki);
+        }
+    }       
+
+    public class RootObject
+    {
+        public string ammattiala { get; set; }
+        public string linkki { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0}", ammattiala);
+        }
+    }
+}
---
+++ Esa/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Windows;
+
+namespace Esa
+{   
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>    
+    public partial class MainWindow : Window
+    {
+        HttpClient webClient = new HttpClient();
+        string valinta = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadItems();            
+        }
+        
+        //Haetaan JSON ja deserialisoidaan, jonka jälkeen näytetään data listboxissa ja buttosella datagridissä.
+        private async void LoadItems()
+        {            
+            try
+            {
+                if (valinta == "") 
+                {
+                    Uri uri = new Uri("http://gis.vantaa.fi/rest/tyopaikat/v1/");
+                    HttpResponseMessage response = await webClient.GetAsync(uri);
+                    string jsonString = await response.Content.ReadAsStringAsync();
+                    List<RootObject> ammattialat = JsonConvert.DeserializeObject<List<RootObject>>(jsonString);
+                    listBox.ItemsSource = ammattialat;
+                }
+                else
+                {
+                    Uri uri = new Uri("http://gis.vantaa.fi/rest/tyopaikat/v1/" + valinta);
+                    HttpResponseMessage response = await webClient.GetAsync(uri);
+                    string jsonString = await response.Content.ReadAsStringAsync();
+                    List<Ammattiala> työpaikka = JsonConvert.DeserializeObject<List<Ammattiala>>(jsonString);
+                    dgResponse.ItemsSource = työpaikka;
+                }               
+            }
+            catch (Exception ex)
+            {
+                debug.Content = ex.Message.ToString();
+            }
+        }
+                
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            valinta = listBox.SelectedItem.ToString();
+            LoadItems();
+            if (debug.Content != null)
+            {
+                debug.Content = null;
+            }            
+        }
+
+        private void btnOhje_Click(object sender, RoutedEventArgs e)
+        {
+            debug.Content = "Valitse ammattiala vasemmalta ja paina valitse -painiketta";
+        }
+    }
+} 
